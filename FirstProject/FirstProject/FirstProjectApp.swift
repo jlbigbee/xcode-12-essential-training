52e4bb3e7b0514d56b268b1801724300f3dd22fa
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-23 18:34:10 -0800

    creating your first Xcode project

---
+++ FirstProject/FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Jonathan Bigbee on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
