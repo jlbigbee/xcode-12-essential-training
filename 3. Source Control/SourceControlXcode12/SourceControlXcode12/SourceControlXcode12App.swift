208dd1f4424d9ca309f368fbcbd01886b498cc28
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-24 12:03:00 -0800

    created a local repository

---
+++ 3. Source Control/SourceControlXcode12/SourceControlXcode12/SourceControlXcode12App.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlXcode12App.swift
+//  SourceControlXcode12
+//
+//  Created by Jonathan Bigbee on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlXcode12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
