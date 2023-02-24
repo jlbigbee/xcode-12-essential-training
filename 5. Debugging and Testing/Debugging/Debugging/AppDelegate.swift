7cabdd8bbc633a97b1f6a82844061dfe8a041d14
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-24 13:40:55 -0800

    used the debugger

---
+++ 5. Debugging and Testing/Debugging/Debugging/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Debugging
+//
+//  Created by Todd Perkins on 12/2/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 5. Debugging and Testing/Debugging/Debugging/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Debugging
+//
+//  Created by Todd Perkins on 12/2/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var resultLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if let num = Int(textField.stringValue) {
+            resultLabel.stringValue = "Square is \(getSquare(num: num))"
+            print(num)
+        } else {
+            resultLabel.stringValue = "Please enter a numeric value"
+        }
+    }
+    
+    func getSquare(num: Int) -> Int {
+        return num * num
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
