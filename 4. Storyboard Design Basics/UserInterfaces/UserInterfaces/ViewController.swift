d33696f21d3f0713343995b2691f1dacaa29462f
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-02-24 12:55:27 -0800

    connected a label

--- 4. Storyboard Design Basics/UserInterfaces/UserInterfaces/ViewController.swift
+++ 4. Storyboard Design Basics/UserInterfaces/UserInterfaces/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label.text = "Hello"
     }
 
 
